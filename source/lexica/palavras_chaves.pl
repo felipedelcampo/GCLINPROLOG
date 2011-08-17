f6b9a833450953e65bc739e93fbac3c38a678085
Felipe Del Campo <delcampofelipe@yahoo.com.br>
2011-08-16 22:20:11 -0300

    Concatenada lista de atons nas palavras chaves

--- source/lexica/palavras_chaves.pl
+++ source/lexica/palavras_chaves.pl
@@ -6,32 +6,32 @@
 
 %%% verifica se id eh na verdade palavra_chave
 palavra_chave([_, 'module', PosX, PosY], [palavra_chave, 'module', PosX, PosY]).
-palavra_chave([_, [p,r,i,v,a,t,e], PosX, PosY], [palavra_chave, 'private', PosX, PosY]).
-palavra_chave([_, [e,n,d], PosX, PosY], [palavra_chave, [e,n,d], PosX, PosY]).
-palavra_chave([_, [c,o,n,s,t], PosX, PosY], [palavra_chave, [c,o,n,s,t], PosX, PosY]).
+palavra_chave([_, 'private', PosX, PosY], [palavra_chave, 'private', PosX, PosY]).
+palavra_chave([_, 'end', PosX, PosY], [palavra_chave, 'end', PosX, PosY]).
+palavra_chave([_, 'const', PosX, PosY], [palavra_chave, 'const', PosX, PosY]).
 palavra_chave([_, 'Boolean', PosX, PosY], [palavra_chave, 'Boolean', PosX, PosY]).
 palavra_chave([_, 'integer', PosX, PosY], [palavra_chave, 'integer', PosX, PosY]).
-palavra_chave([_, [b,e,g,i,n], PosX, PosY, [palavra_chave, 'begin', PosX, PosY]).
-palavra_chave([_, [t,y,p,e,d,e,f], PosX, PosY], [palavra_chave, [t,y,p,e,d,e,f], PosX, PosY]).
-palavra_chave([_, [a,r,r,a,y], PosX, PosY], [palavra_chave, [a,r,r,a,y], PosX, PosY]).
-palavra_chave([_, [r,a,n,g,e], PosX, PosY], [palavra_chave, [r,a,n,g,e], PosX, PosY]).
-palavra_chave([_, [p,r,o,c], PosX, PosY], [palavra_chave, [p,r,o,c], PosX, PosY]).
-palavra_chave([_, [v,a,l], PosX, PosY], [palavra_chave, [v,a,l], PosX, PosY]).
-palavra_chave([_, [r,e,f], PosX, PosY], [palavra_chave, [r,e,f], PosX, PosY]).
-palavra_chave([_, [r,e,t,u,r,n], PosX, PosY], [palavra_chave, [r,e,t,u,r,n], PosX, PosY]).
-palavra_chave([_, [w,r,i,t,e], PosX, PosY], [palavra_chave, [w,r,i,t,e], PosX, PosY]).
-palavra_chave([_, [r,e,a,d], PosX, PosY], [palavra_chave, [r,e,a,d], PosX, PosY]).
-palavra_chave([_, [i,f], PosX, PosY], [palavra_chave, [i,f], PosX, PosY]).
-palavra_chave([_, [f,i], PosX, PosY], [palavra_chave, [f,i], PosX, PosY]).
-palavra_chave([_, [d,o], PosX, PosY], [palavra_chave, [d,o], PosX, PosY]).
-palavra_chave([_, [o,d], PosX, PosY], [palavra_chave, [o,d], PosX, PosY]).
-palavra_chave([_, [t,r,u,e], PosX, PosY], [palavra_chave, [t,r,u,e], PosX, PosY]).
-palavra_chave([_, [f,a,l,s,e], PosX, PosY], [palavra_chave, [f,a,l,s,e], PosX, PosY]).
-palavra_chave([_, [f,o,r,a,l,l], PosX, PosY], [palavra_chave, [f,o,r,a,l,l], PosX, PosY]).
-palavra_chave([_, [l,l,a,r,o,f], PosX, PosY], [palavra_chave, [l,l,a,r,o,f], PosX, PosY]).
-palavra_chave([_, [s,k,i,p], PosX, PosY], [palavra_chave, [s,k,i,p], PosX, PosY]).
-palavra_chave([_, [r,e,a,l], PosX, PosY], [palavra_chave, [r,e,a,l], PosX, PosY]).
-palavra_chave([_, [s,t,r,i,n,g], PosX, PosY], [palavra_chave, [s,t,r,i,n,g], PosX, PosY]).
+palavra_chave([_, 'begin', PosX, PosY, [palavra_chave, 'begin', PosX, PosY]).
+palavra_chave([_, 'typedef', PosX, PosY], [palavra_chave, 'typedef', PosX, PosY]).
+palavra_chave([_, 'array', PosX, PosY], [palavra_chave, 'array', PosX, PosY]).
+palavra_chave([_, 'range', PosX, PosY], [palavra_chave, 'range', PosX, PosY]).
+palavra_chave([_, 'proc', PosX, PosY], [palavra_chave, 'proc', PosX, PosY]).
+palavra_chave([_,  'val', PosX, PosY], [palavra_chave, 'val', PosX, PosY]).
+palavra_chave([_, 'ref', PosX, PosY], [palavra_chave, 'ref', PosX, PosY]).
+palavra_chave([_, 'return', PosX, PosY], [palavra_chave, 'return', PosX, PosY]).
+palavra_chave([_, 'write', PosX, PosY], [palavra_chave, 'write', PosX, PosY]).
+palavra_chave([_, 'read', PosX, PosY], [palavra_chave, 'read', PosX, PosY]).
+palavra_chave([_, 'if', PosX, PosY], [palavra_chave, 'if', PosX, PosY]).
+palavra_chave([_, 'fi', PosX, PosY], [palavra_chave, 'fi', PosX, PosY]).
+palavra_chave([_, 'do', PosX, PosY], [palavra_chave, 'do', PosX, PosY]).
+palavra_chave([_, 'od', PosX, PosY], [palavra_chave, 'od', PosX, PosY]).
+palavra_chave([_, 'true', PosX, PosY], [palavra_chave, 'true', PosX, PosY]).
+palavra_chave([_, 'false', PosX, PosY], [palavra_chave, 'false', PosX, PosY]).
+palavra_chave([_, 'forall', PosX, PosY], [palavra_chave, 'forall', PosX, PosY]).
+palavra_chave([_, 'llarof', PosX, PosY], [palavra_chave, 'llarof', PosX, PosY]).
+palavra_chave([_, 'skip', PosX, PosY], [palavra_chave, 'skip', PosX, PosY]).
+palavra_chave([_, 'real', PosX, PosY], [palavra_chave, 'real', PosX, PosY]).
+palavra_chave([_, 'string', PosX, PosY], [palavra_chave, 'string', PosX, PosY]).
 
 %%% nao eh palavra chave
 palavra_chave(Token, Token).
